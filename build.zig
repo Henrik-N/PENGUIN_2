e3df001d87e95440a3e18e5ceca6dcd9633e4c21
Henrik Nilsson <n.henrik.g.nilsson@gmail.com>
2023-02-08 20:21:54 +0100

    updated build script to work with the latest version of zig

--- build.zig
+++ build.zig
@@ -3,19 +3,19 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     b.prominent_compile_errors = true;
 
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable(.{
+        .name = "PENGUIN",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const exe = b.addExecutable("PENGUIN", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    // This declares intent for the executable to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running 'zig build')
     exe.install();
 
     // C
@@ -28,19 +28,34 @@ pub fn build(b: *std.build.Builder) void {
         exe.linkSystemLibrary("xkbcommon");
     }
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+    // Run step
+    {
+        // This *creates* a RunStep in the build graph, to be executed when another
+        // step is evaluated that depends on it. The line below will establish
+        // such a dependency.
+        const run_cmd = exe.run();
+
+        // Run from installation directory rather than cache directory.
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // This allows the user to pass arguments to the application in the build command
+        // itself, like this: zig build run -- arg1 arg2 etc
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    // Unit testing step
+    {
+        const exe_tests = b.addTest(.{
+            .root_source_file = .{ .path = "src/main.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&exe_tests.step);
+    }
 }
